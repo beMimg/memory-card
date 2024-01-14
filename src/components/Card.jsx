cf80f81500102cd2da7758fd7963a4df3c70c91b
beMimg <bernardommguerreiro@gmail.com>
2024-01-14 13:28:49 +0100

    hovers and animations

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,34 @@
 import "../style/card.css";
+import Tilt from "react-parallax-tilt";
+import { useState, useEffect } from "react";
 
 export function Card({ character, handleClick }) {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(true);
+  }, []);
+
   return (
-    <div className="card" onClick={() => handleClick(character.id)}>
-      <div className="card-img-container">
-        <img className="card-img" src={character.image} alt="charater image" />
-      </div>
-      <div className="card-content">
-        <div className="card-header">
-          <p className="card-header-item-left">{character.race}</p>
-          <p className="card-header-item-right">{character.ki}</p>
+    <Tilt className={`tilt ${show ? "show" : ""}`}>
+      <div className="card" onClick={() => handleClick(character.id)}>
+        <div className="card-img-container">
+          <img
+            className="card-img"
+            src={character.image}
+            alt="charater image"
+          />
         </div>
-        <div className="card-title-container">
-          <h1 className="card-title">{character.name}</h1>
+        <div className="card-content">
+          <div className="card-header">
+            <p className="card-header-item-left">{character.race}</p>
+            <p className="card-header-item-right">{character.ki}</p>
+          </div>
+          <div className="card-title-container">
+            <h1 className="card-title">{character.name}</h1>
+          </div>
         </div>
       </div>
-    </div>
+    </Tilt>
   );
 }
